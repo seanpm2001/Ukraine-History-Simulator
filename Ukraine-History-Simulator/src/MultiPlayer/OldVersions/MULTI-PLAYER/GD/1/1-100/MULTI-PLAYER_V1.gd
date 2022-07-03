2a73689c17451a12d09b42cf1ffee5a86cbc43ae
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-03 15:19:36 -0700

    MULTI-PLAYER_V1.gd
    An archive of the first version of the multiplayer mode script for UHE.

---
+++ Ukraine-History-Simulator/src/MultiPlayer/OldVersions/MULTI-PLAYER/GD/1/1-100/MULTI-PLAYER_V1.gd
@@ -0,0 +1,18 @@
+# Start of script
+# Multiplayer support source code for the Ukraine History Simulator
+# This is pseudocode at the moment, and is not functional
+# Era empty = I haven't done the research to add in events to jump to yet
+func _multiPlayerMode():
+    print("Ukraine History Simulator (multiplayer mode)")
+    print("Choose a side:\nNote:\nIf you pikc a country other than Ukraine, the game will be limited to a certain set of time periods, as not all states are compatible with Ukraines entire history\n")
+    print("🇺🇦️ Ukraine (UK)\n🇷🇺️ Russia (RU)\n🇧🇾️ Belarus (BY)\n🇵🇱️ Poland (PL)\n🇷🇴️ Romania (RO)\n🇲🇩️ Moldova (MD)\n🇩🇪️ Germany (DE)")
+    print("\nThis feature is coming soon")
+    break
+return _multiPlayerMode()
+break  
+# File info
+# File type: GDScript source file (*.gd)
+# File version: 1 (2022, Sunday, July 3rd at 3:19 pm PST)
+# Line count (including blank lines and compiler line): 19
+
+# End of script
