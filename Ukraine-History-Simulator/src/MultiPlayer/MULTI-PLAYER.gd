5f44807286d73bfe7542dd3fe1c341c0741deaf0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-06 00:19:03 -0700

    MULTI-PLAYER.gd [V2]
    Version 2 of the multiplayer mode script for the game.

--- Ukraine-History-Simulator/src/MultiPlayer/MULTI-PLAYER.gd
+++ Ukraine-History-Simulator/src/MultiPlayer/MULTI-PLAYER.gd
@@ -4,15 +4,27 @@
 # Era empty = I haven't done the research to add in events to jump to yet
 func _multiPlayerMode():
     print("Ukraine History Simulator (multiplayer mode)")
-    print("Choose a side:\nNote:\nIf you pikc a country other than Ukraine, the game will be limited to a certain set of time periods, as not all states are compatible with Ukraines entire history\n")
-    print("🇺🇦️ Ukraine (UK)\n🇷🇺️ Russia (RU)\n🇧🇾️ Belarus (BY)\n🇵🇱️ Poland (PL)\n🇷🇴️ Romania (RO)\n🇲🇩️ Moldova (MD)\n🇩🇪️ Germany (DE)")
+    print("Choose a side:\nNote:\nIf you pick a country other than Ukraine, the game will be limited to a certain set of time periods, as not all states are compatible with Ukraines entire history\n")
+    print("🇺🇦️ Ukraine (UK)\n🇷🇺️ Russia (RU)\n🇧🇾️ Belarus (BY)\n🇵🇱️ Poland (PL)\n🇱🇹️ Lithuania (LT)\n🇷🇴️ Romania (RO)\n🇲🇩️ Moldova (MD)\n🇩🇪️ Germany (DE)")
     print("\nThis feature is coming soon")
     break
 return _multiPlayerMode()
-break  
+break
+# Things to account for (gameplay variables)
+"""
+Macros
+Time lapse
+Law/legal
+Military history
+Borders and land
+Other/unlisted
+"""
+
+# V2: Added Lithuania (history period: Polish-Lithuanian commonwealth) I originally forgot to add it, and I keep remembering it at random times during the night. Since I am already pretty distracted right now, I went and added it.
+
 # File info
 # File type: GDScript source file (*.gd)
-# File version: 1 (2022, Sunday, July 3rd at 3:19 pm PST)
-# Line count (including blank lines and compiler line): 19
+# File version: 2 (2022, Wednesday, July 6th at 12:19 pm PST)
+# Line count (including blank lines and compiler line): 31
 
 # End of script
